518f51e9e35414170e3c4c35d807fb18b746ff5e
Arthur Karganyan <arthur.karganyan@gmail.com>
2020-02-07 11:40:31 +0100

    load dotenv, fix and added comment

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require "bundler/setup"
 Bundler.require(:default)
 require 'capybara/dsl'
+require 'dotenv/load'
 
 Capybara.run_server = false
 Capybara.default_driver = :selenium_chrome
@@ -9,6 +10,9 @@ class Browser
     include Capybara::DSL
 
     def init
+        # visit "https://google.com"
         binding.pry
     end
 end
+
+Browser.new.init
